23b0e60a58b5b33921c3809a59947a5b708b580b
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-25 17:28:43 -0700

    finished assignment

--- simple-calc/ViewController.swift
+++ simple-calc/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var result: UITextField!
+    public final let operators : [String] = ["+", "-", "x", "/", "%", "count", "avg", "fact"]
+    public var oprtr: ((Int, Int) -> Int)? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,35 +24,87 @@ class ViewController: UIViewController {
     }
     
     @IBAction func one(_ sender: AnyObject) {
+        result.text! += "1"
     }
     
     @IBAction func two(_ sender: AnyObject) {
+        result.text! += "2"
     }
     
     @IBAction func three(_ sender: AnyObject) {
+        result.text! += "3"
     }
     
     @IBAction func four(_ sender: AnyObject) {
+        result.text! += "4"
     }
     
     @IBAction func five(_ sender: AnyObject) {
+        result.text! += "5"
     }
     
     @IBAction func six(_ sender: AnyObject) {
+        result.text! += "6"
     }
     
     @IBAction func seven(_ sender: AnyObject) {
+        result.text! += "7"
     }
     
     @IBAction func eight(_ sender: AnyObject) {
+        result.text! += "8"
     }
     
     @IBAction func nine(_ sender: AnyObject) {
+        result.text! += "9"
     }
     
     @IBAction func zero(_ sender: AnyObject) {
+        if result.text != nil && result.text != "" {
+            result.text! += "0"
+        }
+    }
+    
+    @IBAction func count(_ sender: AnyObject) {
+        result.text! += " count "
+    }
+    
+    @IBAction func fact(_ sender: AnyObject) {
+        result.text! += " fact "
+    }
+    
+    @IBAction func avg(_ sender: AnyObject) {
+        result.text! += " avg "
+    }
+    
+    @IBAction func add(_ sender: AnyObject) {
+        result.text! += " + "
+        oprtr = add
+    }
+    
+    @IBAction func subtract(_ sender: AnyObject) {
+        result.text! += " - "
+        oprtr = subtract
+    }
+    
+    @IBAction func multiply(_ sender: AnyObject) {
+        result.text! += " * "
+        oprtr = multiply
+    }
+    
+    @IBAction func divide(_ sender: AnyObject) {
+        result.text! += " / "
+    }
+    
+    @IBAction func modulo(_ sender: AnyObject) {
+        result.text! += " % "
+        oprtr = mod
     }
 
+    @IBAction func clear(_ sender: AnyObject) {
+        result.text! = ""
+    }
+    
     func add(left : Int, right : Int) -> Int {
         return left + right
     }
@@ -65,6 +119,14 @@ class ViewController: UIViewController {
     
     func divide(left : Int, right : Int) -> Int {
         return left / right
+    }
+    
+    func mod(left : Int, right : Int) -> Int {
+        return left % right;
+    }
+    
+    func mathOperation(left : Int, right : Int, operation: (Int, Int) -> Int) -> Int {
+        return operation(left, right);
     }
     
     func count(nums: [Int]) -> Int {
@@ -90,6 +152,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculate(_ sender: AnyObject) {
+        let line = result.text!
+        var answer : Int = 0;
+        if (line.contains("count") || line.contains("avg") || line.contains("fact")) {
+            let nums : [String] = line.characters.split(separator: " ").map(String.init);
+            
+            var numbers : [Int] = [];
+            for i in 0...nums.count - 1 {
+                if (!operators.contains(String(nums[i]))) {
+                    numbers.append(Int(nums[i])!);
+                }
+            }
+            
+            if (line.contains("count")) {
+                answer = count(nums: numbers);
+            } else if (line.contains("avg")) {
+                answer = avg(nums: numbers);
+            } else if (line.contains("fact")) {
+                answer = fact(num: numbers[0]);
+            }
+            result.text! = String(answer)
+        } else {
+            let nums = line.characters.split(separator: " ")
+            let firstNum : Int = Int(String(nums[0]))!
+            let secondNum : Int = Int(String(nums[2]))!
+            result.text! = String(mathOperation(left: firstNum, right: secondNum, operation: oprtr!))
+        }
     }
 }
 
